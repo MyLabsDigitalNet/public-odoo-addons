a66c84d65ca7bb88dd389baff17ad8a35e4ba9ae
AdrianBernal <a2b1987@gmail.com>
2025-07-18 19:51:13 +0200

    [IMP] select date to use at import

--- account_statement_import_online_bankifai/__manifest__.py
+++ account_statement_import_online_bankifai/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "Online Bank Statements: BankifAI",
-    "version": "15.0.1.0.0",
+    "version": "15.0.4.0.0",
+    "version": "16.0.4.0.0",
     "category": "Account",
     "author": "Adrián Bernal Bermejo, Ekodo (MyLabs Digital Net SL)",
     "website": "https://qsimov.ekodo.es",
